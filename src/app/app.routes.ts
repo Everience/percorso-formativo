8819b00b18eccdbacc1274fc255c988f85a811fd
rabbi-hossain-ev <rabbi.hossain@everience.com>
2026-02-12 17:28:45 +0100

    feat: restructure app layout and add dynamic routing for tech page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tech',
+    loadComponent: () =>
+      import('./roadmaps/tech/tech')
+        .then(m => m.Tech)
+  },
+  {
+    path: '**',
+    redirectTo: 'tech',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/roadmaps/tech/tech.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { GlassElem } from '../../components/glass-elem/glass-elem';
+
+@Component({
+  selector: 'app-tech',
+  imports: [GlassElem],
+  templateUrl: './tech.html',
+  styleUrl: '../roadmap.scss',
+})
+export class Tech {
+
+}
