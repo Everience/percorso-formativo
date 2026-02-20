082412dc005c5163223ea73ff7416e449c22ed01
Andrea <andrea.grillo@everience.com>
2026-02-20 16:53:56 +0100

    chore: update .gitignore to include firebase.config.ts and retain firebase-key.json

--- src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBMRVwYzJnMLu1yHBpE83qru9YmVBxopjs",
-  authDomain: "everience-crm.firebaseapp.com",
-  projectId: "everience-crm",
-  storageBucket: "everience-crm.firebasestorage.app",
-  messagingSenderId: "321596356788",
-  appId: "1:321596356788:web:c9400c3f4def89f540ffe1"
-};
