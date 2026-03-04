d3ec99321fe12e86e7c19be3c08849d1c36a819f
rabbi-hossain-ev <rabbi.hossain@everience.com>
2026-03-04 15:23:35 +0100

    chore: switch server configuration from production to development environments

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,29 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewareconst
+// Middleware
+/* PRODUCTION */
+/*
 corsOptions = {
   origin: 'https://everience-percorso-formativo.netlify.app',
 };
 app.use(cors(corsOptions));
+*/
+/* DEVELOPMENT */
+app.use(cors());
+
 app.use(bodyParser.json());
 
 const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const admin = require('firebase-admin');
 
+/* PRODUCTION */
+/*
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+*/
+/* DEVELOPMENT */
+const serviceAccount = require('./firebase-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -28,9 +39,9 @@ app.use('/api/courses', courseRoutes);
 
 // Test route
 app.get('/', (req, res) => {
-    res.send('API Training Path Funzionanti');
+  res.send('API Training Path Funzionanti');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server attivo sulla porta ${PORT}`);
+  console.log(`Server attivo sulla porta ${PORT}`);
 });
